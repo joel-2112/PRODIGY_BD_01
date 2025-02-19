3ca28ba35d1057c4e03c2af9775d9d0b7dd709f1
joel-2112 <eyueljoel21@gmail.com>
2025-02-18 22:52:49 -0800

    task01 completed

--- base/views.py
+++ base/views.py
@@ -8,16 +8,13 @@ from .serializers import UserSerializer
 from .storage import UserStorage
 
 class UserListCreate(APIView):
-    """List all users or create a new user."""
     
     def get(self, request):
-        """List all users."""
         users = UserStorage.get_all_users()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
     
     def post(self, request):
-        """Create a new user."""
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user_data = serializer.validated_data
@@ -27,10 +24,8 @@ class UserListCreate(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserRetrieveUpdateDestroy(APIView):
-    """Retrieve, update, or delete a user instance."""
     
     def get_object(self, pk):
-        """Retrieve a user instance."""
         try:
             return UserStorage.get_user(pk)
         except KeyError:
